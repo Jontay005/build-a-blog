28dc469f6012a90cb407ee4884fc1579e6f77479
Jontay <johnwakaba@gmail.com>
2017-10-17 20:12:29 -0500

    added single page and functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,30 +24,36 @@ def index():
 
     return render_template('blog.html', title="Build A Blog!!!!!") 
     
-@app.route("/blog", methods=["POST", "GET"])   
+@app.route("/blog", methods=["GET"])   
 def blog():
 
-    
-    if request.method == 'POST':
-        blog_title = request.form['blog_title']
-        body = request.form['body']
-        new_blog = Blog(blog_title, body)
-
-        db.session.add(new_blog)
-
-        db.session.commit()
-    #last_blog_entry = Blog.query.filter_by(id=10)
     blogs = Blog.query.all()
+    return render_template('blog.html', title="Build A Blog", blogs=blogs)
     
-    return render_template('blog.html', title="Build A Blog", blogs=blogs) 
-    return redirect('each-blog.html')
-    return last_blog_entry 
+
+@app.route("/blogpost", methods=["GET"])
+def blogpost():
+
+    blog_id = request.args.get('id')
+    blog = Blog.query.filter_by(id=blog_id).first()
+
+    return render_template('blogpost.html', blog=blog)
+
     
 
 @app.route("/newpost", methods=["POST", "GET"])    
 def newpost():
-    return render_template('newpost.html')
 
-
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        body = request.form['body']
+        new_blog = Blog(blog_title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+        id = new_blog.id
+        return redirect(url_for("blogpost", id=id)) #pass primitive type
+    else:
+        return render_template("/newpost.html")
+    
 if __name__ == '__main__':
     app.run()
